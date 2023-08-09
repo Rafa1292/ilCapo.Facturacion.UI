cc9df87a79844b325d004823e4c5fcf18236c595
rafa1292 <rvilla3452@gmail.com>
2023-08-09 14:20:43 -0600

    changes

--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -27,9 +27,9 @@ const Login = () => {
 
   return (
     <div className='col-12 d-flex flex-wrap justify-content-center align-items-center' style={{ height: '100vh' }}>
-      <form className='col-4 d-flex flex-wrap justify-content-center p-4 login' onSubmit={() => login(user)}>
+      <form className='col-4 d-flex flex-wrap justify-content-center p-4 login'>
         <img className="mb-4" src={logoNebulosa} alt="" width="200" height="70" />
-        <h1 className="h3 mb-3 fw-normal col-12 text-center">Inicio de sesion</h1>
+        <h1 className="h3 mb-3 fw-normal col-12 text-center">Iniciar sesion</h1>
         <div className="form-floating">
           <input type="email" name='email' className="form-control" onChange={handleChange} id="floatingInput" placeholder="name@example.com" />
           <label htmlFor="floatingInput">Correo</label>
@@ -38,7 +38,7 @@ const Login = () => {
           <input type="password" name='password' className="form-control" onChange={handleChange} id="floatingPassword" placeholder="Password" />
           <label htmlFor="floatingPassword">Contraseña</label>
         </div>
-        <button className="col-12 btn btn-lg" type="submit">Ingresar</button>
+        <button className="col-12 btn btn-lg" type="button" onClick={()=> login(user)}>Entrar</button>
         <small className='col-12 text-center my-4'>¿Olvidaste tu contraseña?</small>
       </form>
     </div>
