301b2052c8c72eef5f58b9418c04320fc23ca381
rafa1292 <rvilla3452@gmail.com>
2023-08-10 18:52:57 -0600

    impresion correcta

--- src/hooks/useBill.ts
+++ src/hooks/useBill.ts
@@ -434,6 +434,7 @@ const useBill = (): BillFunctions => {
   const printCommand = async (commandBill: Bill) => {
     try {
       const response = await axios.post('https://localhost:5000/command', commandBill)
+      console.log(commandBill)
       if (response.data === false) {
         Swal.fire({
           icon: 'error',
